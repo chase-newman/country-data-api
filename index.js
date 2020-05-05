795458b1656383d6c5aa0d59cbfc1a4f70f8425c
EC2 Default User <ec2-user@ip-172-31-44-171.ec2.internal>
2020-05-05 20:00:27 +0000

    update port

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 var express = require("express");
 var app = express();
+var PORT = process.env.PORT || 5000;
 
 
 app.get("/", (req,res) => {
     res.json(["chase", "jordan", "newman"]); 
 });
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
    console.log("Server Running On Port 8080") 
 });
