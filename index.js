0c3f87f5d3151407c2a6538b8fea6a65fab61f06
EC2 Default User <ec2-user@ip-172-31-44-171.ec2.internal>
2020-05-05 19:57:21 +0000

    update port to 5000

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ app.get("/", (req,res) => {
     res.json(["chase", "jordan", "newman"]); 
 });
 
-app.listen(3000, () => {
+app.listen(5000, () => {
    console.log("Server Running On Port 8080") 
 });
